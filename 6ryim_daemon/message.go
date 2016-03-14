fef65ff172088c75812d72c2f9a4d85a26b08987
kyf <kyf456@163.com>
2016-03-14 16:27:55 +0800

    debug

--- 6ryim_daemon/message.go
+++ 6ryim_daemon/message.go
@@ -79,6 +79,7 @@ func newMsg(m []byte) (*Message, error) {
 		result.SystemType = "0"
 	}
 
+	fmt.Println("IsSystem is ", result.IsSystem, ", SystemType is ", result.SystemType)
 	return &result, nil
 }
 
